848ba661c3978691fc62bed97f4f8fe8e67536a2
Amanda Perch <perch_amanda@hotmail.com>
2023-05-23 10:24:00 -0300

    Parametros por referencia

---
+++ Avançado/parametros_por_referencia/Program.cs
@@ -0,0 +1,22 @@
+﻿static void AlterarRef(ref int numero)
+{ 
+  numero = numero + 500;
+}
+static void AlteraOut(int numero)
+{
+  numero = 0;
+  numero = numero + 50;
+}
+static void Main()
+{
+  int x = 5;
+  AlterarRef(ref x);
+  Console.WriteLine(x);
+
+  int y = 8;
+  AlteraOut(y);
+  Console.WriteLine(y);
+
+}
+
+Main();
