927c461ba2414b9b7433f118aeed21d6282901ba
Amanda Perch <perch_amanda@hotmail.com>
2023-05-23 08:43:14 -0300

    Parametros Variaveis

---
+++ Avançado/parametros_variaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿static void Despedida(params string[] alunos)
+{
+   foreach (var aluno in alunos) 
+   {
+     Console.WriteLine("Tchau {0}, até a próxima aula", aluno);
+   }
+}
+
+static void Main()
+{
+  Despedida("joao", "thiago", "Antonio");
+}
+
+Main();
