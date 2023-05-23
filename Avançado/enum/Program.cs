4c4a4e905dfcbd071f55e13ee88bad8c0329b9f3
Amanda Perch <perch_amanda@hotmail.com>
2023-05-23 09:05:49 -0300

    Enum

---
+++ Avançado/enum/Program.cs
@@ -0,0 +1,17 @@
+﻿class Enum 
+{
+  public enum Genero {Filme, Serie, Documentario};
+
+  public class Filme
+  {
+    public string Nome;
+    public Genero TipoVideo;
+  }
+
+  static void Main()
+  {
+    int cod = (int)Genero.Serie;
+    Console.WriteLine(cod);
+  }
+
+}
