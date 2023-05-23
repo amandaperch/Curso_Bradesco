e7732e4aaf9c13f57d09303c0abfc93d38987040
Amanda Perch <perch_amanda@hotmail.com>
2023-05-23 08:16:27 -0300

    Aprendendo sobre classes, metodos, construtores e elementos esttaticos

---
+++ Avançado/Elementos_estaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿class MetodosEstaticos 
+{
+
+  public static int somar (int x, int y)
+  {
+    return x + y;
+  }
+
+  public static int multiplicar (int x, int y)
+  {
+    return x * y;
+  }
+
+  static void Main (string[] args)
+  {
+    var result = MetodosEstaticos.somar(3,3);
+    Console.WriteLine("A soma é igual a: {0}", result);
+
+    Console.WriteLine(MetodosEstaticos.multiplicar(3,4));
+  }
+}
