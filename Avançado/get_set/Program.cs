513a9b8db46d599ba24f3ffbd2c4b83b2fd4a479
Amanda Perch <perch_amanda@hotmail.com>
2023-05-23 09:00:59 -0300

    Get e set

---
+++ Avançado/get_set/Program.cs
@@ -0,0 +1,42 @@
+﻿class Carro 
+{
+  private string Nome;
+  private string Marca;
+  private double Potencia;
+
+  public Carro(string nome, string marca, double potencia)
+  {
+    Nome = nome;
+    Marca = marca;
+    Potencia = potencia;
+  }
+
+  public Carro()
+  {
+  }
+
+  public string GetMarca()
+  {
+    return Marca;
+  }
+  public void SetMarca(string marca)
+  {
+    Marca = marca;
+  }
+   public string GetNome()
+  {
+    return Nome;
+  }
+  public void SetNome(string nome)
+  {
+    Nome = nome;
+  }
+   public double GetPotencia()
+  {
+    return Potencia;
+  }
+  public void SetPotencia(double potencia)
+  {
+    Potencia = potencia;
+  }
+}
